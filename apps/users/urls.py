1207cf27e039d9be633be4f2f149f0412b2e945f
rafacho <davi.s.rafacho.developer@gmail.com>
2024-01-05 16:29:07 -0300

    feat: adição da nova view de login nas urls

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path, include
+
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import (
     token_obtain_pair as token_obtain,
@@ -6,14 +7,14 @@ from rest_framework_simplejwt.views import (
     token_verify,
 )
 
-from .views import UsuarioViewSet
+from .views import UsuarioViewSet, custom_token_obtain_pair_view
 
 router = DefaultRouter()
 router.register("usuarios", UsuarioViewSet, "usuarios")
 
 urlpatterns = [
     path("", include(router.urls)),
-    path("api/token/obtain/", token_obtain, name="token_obtain"),
+    path("api/token/obtain/", custom_token_obtain_pair_view, name="token_obtain"),
     path("api/token/refresh/", token_refresh, name="token_refresh"),
     path("api/token/verify/", token_verify, name="token_verify"),
 ]
