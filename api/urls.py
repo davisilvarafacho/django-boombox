d637173228afb6604a43cac16e11a56dd0bc21b4
rafacho <davi.s.rafacho.developer@gmail.com>
2024-02-19 14:26:11 -0300

    feat: adição nas de urls para servir arquivos estáticos

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,18 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
+from django.conf.urls.static import static
+
+# Essa variável percorre os aplicativos registrados em `settings.BOOMBOX_APPS`
+# e automáticamente registra o arquivo `urls.py` de app. Isso faz com que seja
+# obrigatório que, todos os apps, tenham um arquivo `urls.py` e dentro dele a
+# variável `urlpatterns` criada.
 
 apps_urls = [path("api/v1/", include(app + ".urls")) for app in settings.BOOMBOX_APPS]
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-] + apps_urls
+    *apps_urls,
+    *static(settings.STATIC_URL, document_root=settings.STATIC_ROOT),
+    *static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
+]
