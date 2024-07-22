c0ed46de83405825a4e11c508961382c5fad7232
davisilvarafacho <davi.s.rafacho.developer@gmail.com>
2024-07-22 17:33:06 -0300

    fix: utilizacao dos parametros

--- apps/system/core/cache.py
+++ apps/system/core/cache.py
@@ -1,8 +1,8 @@
 from cachalot.utils import get_query_cache_key, get_table_cache_key
 
-def gen_query_cache_key():
-    return "db-" + get_query_cache_key()
+def gen_query_cache_key(*args, **kwargs):
+    return "db-" + get_query_cache_key(*args, **kwargs)
 
-def gen_query_table_key():
-    return "db-" + get_table_cache_key()
+def gen_query_table_key(*args, **kwargs):
+    return "db-" + get_table_cache_key(*args, **kwargs)
 
