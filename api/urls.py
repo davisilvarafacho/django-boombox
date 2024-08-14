789dae02459a7642dec27109ceb8963f6c3cfd1b
davisilvarafacho <davi.s.rafacho.developer@gmail.com>
2024-08-14 13:02:22 -0300

    feat: urls de arquivos estáticos serão servidos somente em desenvolvimento

--- api/urls.py
+++ api/urls.py
@@ -1,13 +1,15 @@
-from django.contrib import admin
-from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
 
 apps_urls = [path("api/", include(app + ".urls")) for app in settings.BOOMBOX_APPS]
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     *apps_urls,
-    *static(settings.STATIC_URL, document_root=settings.STATIC_ROOT),
-    *static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
 ]
+
+if settings.IN_DEVELOPMENT:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT),
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
