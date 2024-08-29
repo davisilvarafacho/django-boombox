996c532eeb007c1a843bf03f1b9dfce0ee264c19
davi.s.rafacho <davi.s.rafacho@gmail.com>
2024-08-29 13:51:37 +0000

    feat: mudança do token de constante para variável de ambiente

--- apps/system/tenants/authentications.py
+++ apps/system/tenants/authentications.py
@@ -1,12 +1,12 @@
 from rest_framework.exceptions import AuthenticationFailed
 
-TOKEN_INTEGRACAO = "67104cf3-6c90-42e7-a253-489d645babf6"   
+from utils.env import get_env_var
 
 
 class TokenIntegracaoAuthenticaton:
     def authenticate(self, request):
         token_integracao = request.headers["Authorization"].split(" ")[1]
-        if token_integracao != TOKEN_INTEGRACAO:
+        if token_integracao != get_env_var("TOKEN_INTEGRACAO"):
             raise AuthenticationFailed({"mensagem": "O token de integração não é válido"}, code="invalid_token")
 
         return None, token_integracao
