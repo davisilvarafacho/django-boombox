9eb0bcb4071c63e97a22d52206c07c0d855cf649
davisilvarafacho <davi.s.rafacho.developer@gmail.com>
2024-08-29 01:11:39 +0000

    feat: adição das urls de auto documentação da api

--- api/urls.py
+++ api/urls.py
@@ -3,12 +3,21 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
+
+
 apps_urls = [path("api/", include(app + ".urls")) for app in settings.BOOMBOX_APPS]
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+
     path("accounts/", include("allauth.urls")),
     path("_allauth/", include("allauth.headless.urls")),
+
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    
     *apps_urls,
 ]
 
