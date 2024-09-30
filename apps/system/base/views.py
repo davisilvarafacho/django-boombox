77e03e2d92b740206721a39dcca137b335b8a5e1
davisilvarafacho <davi.s.rafacho.developer@gmail.com>
2024-09-30 17:29:30 +0000

    docs: improve

--- apps/system/base/views.py
+++ apps/system/base/views.py
@@ -2,27 +2,75 @@ from django.db.models import ProtectedError
 
 from rest_framework import status
 from rest_framework.decorators import action
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.viewsets import ModelViewSet, ViewSet
 from rest_framework.response import Response
 
 
-class BaseViewSet(ModelViewSet):
+class BaseViewSet(ViewSet):
     serializer_classes = {}
+    serializer_class = None
     filterset_fields = {}
     search_fields = []
 
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        distinct_fields = self.request.query_params.get("distinct", None)
+        if distinct_fields is not None:
+            return queryset.distinct(*distinct_fields)
+        return queryset
+
     def get_serializer_class(self):
         assert self.serializer_classes != {} or self.serializer_class is not None, (
-            "'%s' deve implementar o 'serializer_class' ou  'serializer_classes'." % self.__class__.__name__
+            "'%s' deve implementar o 'serializer_class' ou  'serializer_classes'."
+            % self.__class__.__name__
         )
 
         if self.serializer_class:
             return self.serializer_class
-        
-        if action is None:
-            action = self.action
 
-        return self.serializer_classes[action]
+        return self.serializer_classes[self.action]
+
+
+class BaseModelViewSet(ModelViewSet):
+    serializer_classes = {}
+    serializer_class = None
+    filterset_fields = {}
+    search_fields = []
+    ordering_fields = []
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        distinct_fields = self.request.query_params.get("distinct", None)
+        if distinct_fields is not None:
+            return queryset.distinct(*distinct_fields)
+        return queryset
+
+    def get_serializer_class(self):
+        assert self.serializer_classes != {} or self.serializer_class is not None, (
+            "'%s' deve implementar o 'serializer_class' ou  'serializer_classes'."
+            % self.__class__.__name__
+        )
+
+        if self.serializer_class:
+            return self.serializer_class
+
+        return self.serializer_classes[self.action]
+
+    def get_object(self):
+        instance = super().get_object()
+        self.instance = instance
+        return instance
+
+    def get_aditional_serializer_context(self):
+        return {}
+
+    def get_serializer_context(self):
+        context = super().get_serializer_context()
+        aditional_context = self.get_aditional_serializer_context()
+        return {"action": self.action, **context, **aditional_context}
+
+    def perform_create(self, serializer, **overwrite):
+        serializer.save(owner=self.request.user, **overwrite)
 
     def destroy(self, request, *args, **kwargs):
         try:
@@ -31,15 +79,10 @@ class BaseViewSet(ModelViewSet):
             return Response(status=status.HTTP_204_NO_CONTENT)
 
         except ProtectedError as ex:
-            return Response({'error': ex.args[0]}, status=status.HTTP_409_CONFLICT)
-
-    @action(methods=["get"], detail=True)
-    def clonar(self, request):
-        instance = self.get_object()
-        instance.pk = None
-        instance.save()
-        serializer = self.get_serializer(instance)
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(
+                {"mensagem": "Esse registro já foi utilizado pelo sistema"},
+                status=status.HTTP_409_CONFLICT,
+            )
 
     @action(methods=["post"], detail=False)
     def bulk_create(self, request):
@@ -47,4 +90,20 @@ class BaseViewSet(ModelViewSet):
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
         headers = self.get_success_headers(serializer.data)
-        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+        return Response(
+            serializer.data, status=status.HTTP_201_CREATED, headers=headers
+        )
+
+    @action(methods=["get"], detail=True)
+    def clonar(self, request, pk):
+        instance = self.get_object()
+        instance.pk = None
+        self.alterar_campos_unicos(instance)
+        instance.save()
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    def alterar_campos_unicos(self, instance):
+        """Método para alterar campos com `unique=True` na action `clonar
+        """
+        pass
