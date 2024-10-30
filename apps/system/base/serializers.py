1bb7df4e03d1065f7ea9fcd43c304bef0fc7731a
Davi Silva Rafacho <davi.s.rafacho.developer@gmail.com>
2024-10-30 16:33:23 -0300

    feat: criação do serializer base

--- apps/system/base/serializers.py
+++ apps/system/base/serializers.py
@@ -1,16 +1,31 @@
-from rest_framework import serializers
+from rest_framework.serializers import ModelSerializer, PrimaryKeyRelatedField
 
 from apps.users.serializers import OnwerSerializer
+from apps.system.tenants.serializers import TenantSerializer
 
 
-class BaseModelSerializer(serializers.ModelSerializer):
+class BaseModelSerializer(ModelSerializer):
+    empresa = TenantSerializer(read_only=True)
     owner = OnwerSerializer(read_only=True)
-    empresa = serializers.PrimaryKeyRelatedField(read_only=True)
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def get_fields(self):
+        fields = super().get_fields()
+        action = self.context.get("action", None)
 
-        action = self.context["action"]
         if action in ("list", "retrieve"):
-            for field_name in self.get_fields():
-                self.fields[field_name].read_only = True
+            for field_name in fields:
+                fields[field_name].read_only = True
+
+        elif action in ("create", "update", "partial_update"):
+            for field_name in fields:
+                field_cls = fields[field_name].__class__
+                if issubclass(field_cls, ModelSerializer):
+                    model = field_cls.Meta.model
+                    method = getattr(self, f"get_{field_name}_queryset", None)
+                    if method:
+                        fields[field_name] = PrimaryKeyRelatedField(queryset=method(model))
+                    else:
+                        fields[field_name] = PrimaryKeyRelatedField(queryset=model.objects.all())
+            fields["owner"].read_only = True
+
+        return fields
