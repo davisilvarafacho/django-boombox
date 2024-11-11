650fe75d98fe8ce24c088a1fd742c3a9b258b243
Davi Silva Rafacho <rafacho.s.davi@gmail.com>
2024-11-11 10:46:36 -0300

    feat: verbose name como chave dos erros

--- apps/system/base/serializers.py
+++ apps/system/base/serializers.py
@@ -1,5 +1,21 @@
+from rest_framework import serializers
+
 from rest_flex_fields import FlexFieldsModelSerializer
 
 
 class BaseModelSerializer(FlexFieldsModelSerializer):
-    pass
+    def get_field_verbose_name(self, field_name):
+        # Retorna o verbose_name do campo ou o próprio nome do campo se não existir
+        return self.Meta.model._meta.get_field(field_name).verbose_name
+
+    def run_validation(self, data=serializers.empty):
+        try:
+            return super().run_validation(data)
+        except serializers.ValidationError as e:
+            errors = e.detail
+            new_errors = {}
+            for field_name, field_errors in errors.items():
+                # Usa o verbose_name no erro
+                verbose_name = self.get_field_verbose_name(field_name)
+                new_errors[verbose_name] = field_errors
+            raise serializers.ValidationError(new_errors)
