97ca705b9e5027fcc5b3f001adb0e12ab5207834
Davi Silva Rafacho <rafacho.s.davi@gmail.com>
2025-01-21 20:28:01 +0000

    feat: criação do app separado para configurações e personalização do comportamento da api

---
+++ apps/configuracoes/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+from .models import Configuracao
+
+
+@admin.register(Configuracao)
+class ConfiguracaoAdmin(admin.ModelAdmin):
+    list_display = ("ativo", "cf_codigo", "cf_descricao", "cf_valor")
+    list_filter = ("ativo", "cf_codigo", "cf_descricao")
+    search_fields = ("cf_codigo",)
+    ordering = ("cf_codigo",)
