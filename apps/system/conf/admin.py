1293832b5d69a34ffa9a2170c8dc0eb7c4d21bfc
Davi Silva Rafacho <rafacho.s.davi@gmail.com>
2025-01-21 20:27:44 +0000

    feat: reinicio do app de conf

--- apps/system/conf/admin.py
+++ apps/system/conf/admin.py
@@ -1,10 +1,3 @@
 from django.contrib import admin
-from .models import Configuracao
 
-
-@admin.register(Configuracao)
-class ConfiguracaoAdmin(admin.ModelAdmin):
-    list_display = ('ativo', 'cf_codigo', 'cf_descricao', 'cf_valor')
-    list_filter = ('ativo', 'cf_codigo', 'cf_descricao')
-    search_fields = ('cf_codigo',)
-    ordering = ('cf_codigo',)
+# Register your models here.
